094e871b0821c1e0dd6202a3d44cfc58717ae231
Gareth Trufitt <gtrufitt@users.noreply.github.com>
2022-05-24 18:42:36 +0100

    Add test API route

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => res.type('html').send(html));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
+app.get("/post-message", (req, res) => {
+  console.log('post-message called')
+});
+
 
 const html = `
 <!DOCTYPE html>
